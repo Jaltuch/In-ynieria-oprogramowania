2b79bb61e514c5e0277d7270cb0a963ea79297ac
Jaltuch <60828143+Jaltuch@users.noreply.github.com>
2024-03-05 16:57:37 +0100

    Add files via upload
    pliki z 2 zajęć

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Pies.h"
+#include "Jamnik.h"
+#include "Ssak.h"
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    Jamnik jamnik;
+
+    jamnik.Imie="abc";
+    cout<<"cos";
+
+
+
+
+
+    return 0;
+}
