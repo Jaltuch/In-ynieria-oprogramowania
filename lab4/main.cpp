39ebb6e3b77298ad9960959babb57c170552ac69
Jaltuch <60828143+Jaltuch@users.noreply.github.com>
2024-03-19 16:55:05 +0100

    Add files via upload

---
+++ lab4/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Audiobook.cpp"
+#include "Lektor.cpp"
+
+using namespace std;
+
+int main()
+{
+    Audiobook a;
+    Lektor l;
+    l.Czytaj();
+
+
+    return 0;
+}
