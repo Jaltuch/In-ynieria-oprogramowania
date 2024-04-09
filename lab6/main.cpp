0ea81bb47da7f0f83ab10a4753885a23496b88ef
Jaltuch <60828143+Jaltuch@users.noreply.github.com>
2024-04-09 16:58:03 +0200

    Add files via upload

---
+++ lab6/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "USER.cpp"
+//#include "SYSTEM.cpp"
+
+
+using namespace std;
+
+int main()
+{
+    USER user;
+    user.rejestruj();
+    user.login();
+
+
+
+    return 0;
+}
